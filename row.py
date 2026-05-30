c297241502ec708a316dab50b9c2e95a7ab65fc1
Neeraj <neeraj.cseh@gmail.com>
2026-05-30 15:57:28 +0530

    add

--- row.py
+++ row.py
@@ -4,4 +4,8 @@ from requests import head
 # df = pd.read_csv("sales_data_sample.csv", encoding = "latin1")
 # df = pd.read_excel("SampleSuperstore.xlsx")
 df = pd.read_json("sample_Data.json")
+print("Top 4 rows of the DataFrame:")
 print(df.head(4))
+
+print("\nBottom 4 rows of the DataFrame:")
+print(df.tail(4))
